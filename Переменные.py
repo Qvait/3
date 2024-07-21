b0c0839a799415e789b927d317c08001ac8d814b
Qvait <brylov.1996@mail.ru>
2024-07-22 01:14:04 +0500

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,9 @@
+#Количество выполненных ДЗ (запишите значение 12)
+#Количество затраченных часов (запишите значение 1.5)
+#Название курса (запишите значение 'Python')
+#Время на одно задание (вычислить используя 1 и 2 переменные)
+kolichestvo_vypolnenyh_dz=12
+kolichestvo_zatrachenyh_chasov=1.5
+nazvanie_kursa="Pyton"
+Vrema_na_odno_zadanit=kolichestvo_zatrachenyh_chasov/kolichestvo_vypolnenyh_dz
+print(Vrema_na_odno_zadanit)
